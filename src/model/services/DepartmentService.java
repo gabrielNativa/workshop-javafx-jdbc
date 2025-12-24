81550562cc778a2b66a883aadff8e26027c0764b
gabrielNativa <gabrielmoreira450@outlook.com>
2025-12-24 17:52:00 -0300

    Saving a new department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,6 +1,5 @@
 package model.services;
 
-
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -10,11 +9,20 @@ import model.entities.Department;
 public class DepartmentService {
 
 	private DepartmentDaoJDBC dao = (DepartmentDaoJDBC) DaoFactory.createDepartmentDao();
-	
-	public List<Department> findAll(){
-		
-		
+
+	public List<Department> findAll() {
+
 		return dao.findAll();
 	}
-	
+
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		} 
+		else {
+			dao.update(obj);
+
+		}
+	}
+
 }
